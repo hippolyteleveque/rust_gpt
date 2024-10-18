8ca18a327f9f75b2c962889619000c9102deca31
Hippolyte Lévêque <hippolyte.leveque@essec.edu>
2024-10-18 17:53:18 +0200

    Added the dummy tokenizer from the llm book.

---
+++ src/bin/tokenizer.rs
@@ -0,0 +1,44 @@
+use regex::Regex;
+use std::fs;
+
+fn main() {
+    let file_content =
+        fs::read_to_string("./data/the-verdict.txt").expect("The file should be present");
+    println!("The len of the file is {}", file_content.len());
+    println!("The first lines of the fiels are {:?}", &file_content[..99]);
+    let text = "I HAD always thought Jack Gisburn rather";
+    let re =
+        Regex::new(r#"([,.?_!"()\']|--|\s)"#).expect("This should be a valid regular expression.");
+    // println!("The splitted text is : {words:?}");
+
+    let mut words = Vec::new();
+    let mut left = 0;
+    for m in re.find_iter(&text) {
+        let start = m.start();
+        let end = m.end();
+        if text[left..start].trim() != "" {
+            words.push(text[left..start].trim());
+        }
+        if m.as_str().trim() != "" {
+            words.push(m.as_str());
+        }
+        left = end;
+    }
+    println!("The splitted text is : {words:?}");
+    let mut left = 0;
+    let mut preprocessed = Vec::new();
+    for m in re.find_iter(&file_content) {
+        let start = m.start();
+        let end = m.end();
+        if file_content[left..start].trim() != "" {
+            preprocessed.push(file_content[left..start].trim());
+        }
+        if m.as_str().trim() != "" {
+            preprocessed.push(m.as_str());
+        }
+        left = end;
+    }
+
+    println!("Num tokens find in the text: {}", preprocessed.len());
+    println!("The 30 first tokens are: {:?}", &preprocessed[..30]);
+}
